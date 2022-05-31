09e53f6f6168eaf8f675f2ddbbe8255b41448007
Fatma Nur Seval <104559152+nurseval@users.noreply.github.com>
2022-05-31 11:42:57 +0300

    değiştirildi

--- Program.cs
+++ Program.cs
@@ -16,16 +16,39 @@ namespace hatayonetimi
 
             catch(Exception ex)
 
-            {
+           {
                 Console.WriteLine("Hata:"+ ex.Message.ToString());
             }
 
             finally
-            {
+           {
                 Console.Write("İşlem tamamlandı.");
             }
             Console.ReadKey();
-        }
+             
+             /* try
+               {
+                  int a = int.Parse(Console.ReadLine());
+
+                  int b = int.Parse(Console.ReadLine());
+
+                  int c = a+b;
+ 
+                  Console.WriteLine(c);
+                }
+                   catch(Exception ex)
+               {
+                   Console.WriteLine("Bir Hata Oluştu: "+ ex.Message);
+                }
+                  finally
+               {
+                    Console.WriteLine("İşlem tamamlandı.");
+                }*/
+
+
+
+        }  
+    
     }
 }
     
